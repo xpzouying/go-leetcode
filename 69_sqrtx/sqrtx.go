2e29ba3cfe9d78bbfe5b049192a69ace06810f67
zy <xpzouying@gmail.com>
2019-12-05 00:24:40 +0800

    69：优化结果

--- 69_sqrtx/sqrtx.go
+++ 69_sqrtx/sqrtx.go
@@ -1,5 +1,13 @@
 package sqrtx
 
+/**
+
+执行用时 :
+12 ms , 在所有 golang 提交中击败了 22.21% 的用户
+
+内存消耗 :
+2.2 MB , 在所有 golang 提交中击败了 98.04% 的用户
+
 func mySqrt(x int) int {
 	if x == 0 {
 		return 0
@@ -23,4 +31,53 @@ func mySqrt(x int) int {
 	}
 
 	return 0
+}
+*/
+
+/**
+优化思路：
+
+1. x的平方根不会超出x/2
+
+2. 可以使用分治算法找出平方根
+
+具体步骤：
+
+平方根的取值范围：
+[0, x/2]
+
+*/
+func mySqrt(x int) int {
+	if x == 0 {
+		return 0
+	}
+
+	if x < 4 {
+		return 1
+	}
+
+	left := 1
+	right := x >> 1
+
+	for {
+		if left >= right {
+			break
+		}
+
+		mid := (left + right + 1) >> 1
+
+		sqrt := int(mid * mid)
+		if sqrt == x {
+			return mid
+		}
+
+		// 平方太大了，往低位查找
+		if sqrt > x {
+			right = mid - 1
+		} else {
+			left = mid
+		}
+	}
+
+	return left
 }

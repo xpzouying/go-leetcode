ea6f24fb3992e70bc9f08152f1fe617c166881d3
zy <xpzouying@gmail.com>
2019-12-09 15:46:22 +0800

    更新优化后的结果
    增加hash map优化方案

--- 146_lru_cache/lru_cache.go
+++ 146_lru_cache/lru_cache.go
@@ -8,10 +8,11 @@ type Node struct {
 }
 
 type LRUCache struct {
-	len  int   // 当前队列的个数
-	cap  int   // 上限
-	head *Node // 队首节点
-	tail *Node // 指向队尾节点
+	len  int           // 当前队列的个数
+	cap  int           // 上限
+	head *Node         // 队首节点
+	tail *Node         // 指向队尾节点
+	m    map[int]*Node // hashmap，key为key，value为指向双链表中的节点
 }
 
 func Constructor(capacity int) LRUCache {
@@ -19,7 +20,7 @@ func Constructor(capacity int) LRUCache {
 	tail := &Node{Pre: head, Next: nil}
 	head.Next = tail
 
-	return LRUCache{len: 0, cap: capacity, head: head, tail: tail}
+	return LRUCache{len: 0, cap: capacity, head: head, tail: tail, m: make(map[int]*Node, capacity)}
 }
 
 func (this *LRUCache) Println() []int {
@@ -47,93 +48,73 @@ func (this *LRUCache) Get(key int) int {
 	if this.len == 0 {
 		return -1
 	}
-	p := this.head.Next
-	for {
-		if p == this.tail {
-			break
-		}
 
-		if p.Key == key {
-			// 找到当前节点
-			if p.Pre == this.head {
-				// 如果已经是队首，则直接返回
-				return p.Val
-			}
-			// 移动当前节点到队首
-
-			// 1. 从队列中拿出来
-			p.Pre.Next = p.Next
-			p.Next.Pre = p.Pre
-
-			// 2. 挂载到head后面
-			p.Next = this.head.Next
-			p.Pre = this.head
-			this.head.Next.Pre = p
-			this.head.Next = p
-
-			return p.Val
-		}
-
-		p = p.Next
+	// 直接查找hash map，若不存在则直接返回
+	node, ok := this.m[key]
+	if !ok {
+		return -1
 	}
 
-	return -1
+	if node.Pre == this.head {
+		return node.Val
+	}
+
+	// 移动当前节点到队首
+	// 1. 从队列中拿出来
+	node.Pre.Next = node.Next
+	node.Next.Pre = node.Pre
+
+	// 2. 挂载到head后面
+	node.Next = this.head.Next
+	node.Pre = this.head
+	this.head.Next.Pre = node
+	this.head.Next = node
+
+	return node.Val
 }
 
 func (this *LRUCache) Put(key int, value int) {
-	p := this.head.Next
+	node, ok := this.m[key]
+	// 如果存在，则直接更新节点在列表中的位置
+	if ok {
+		// 1. 从队列中拿出来
+		node.Pre.Next = node.Next
+		node.Next.Pre = node.Pre
 
-	for {
-		if p == this.tail {
-			// 没有找到节点
-			break
-		}
+		// 2. 挂载到head后面
+		node.Next = this.head.Next
+		node.Pre = this.head
+		this.head.Next.Pre = node
+		this.head.Next = node
 
-		if p.Key == key {
-			// 找到节点
-			break
-		}
-
-		p = p.Next
-	}
-
-	// 如果没有找到
-	if p == this.tail {
-		// 如果容量已满，则淘汰后，再增加
-		if this.len == this.cap {
-			// 删除队尾元素
-			this.tail.Pre.Pre.Next = this.tail
-			this.tail.Pre = this.tail.Pre.Pre
-		} else {
-			// 如果容量未满
-			this.len++
-		}
-
-		newNode := &Node{
-			Key: key,
-			Val: value,
-			Pre: this.head,
-		}
-		newNode.Next = this.head.Next
-		newNode.Next.Pre = newNode
-		this.head.Next = newNode
+		node.Val = value
 		return
 	}
 
-	// 如果找到了
-	// 移动当前节点到队首
+	// 不存在的情况，需要判断是否超出容量
+	if this.len == this.cap {
+		// 删除队尾元素
 
-	// 1. 从队列中拿出来
-	p.Pre.Next = p.Next
-	p.Next.Pre = p.Pre
+		// 删除hash map中的对应的key
+		key := this.tail.Pre.Key
+		delete(this.m, key)
 
-	// 2. 挂载到head后面
-	p.Next = this.head.Next
-	p.Pre = this.head
-	this.head.Next.Pre = p
-	this.head.Next = p
+		this.tail.Pre.Pre.Next = this.tail
+		this.tail.Pre = this.tail.Pre.Pre
+	} else {
+		// 如果容量未满
+		this.len++
+	}
 
-	p.Val = value
+	newNode := &Node{
+		Key: key,
+		Val: value,
+		Pre: this.head,
+	}
+	newNode.Next = this.head.Next
+	newNode.Next.Pre = newNode
+	this.head.Next = newNode
+	this.m[key] = newNode // 增加hashmap
 }
 
 /**
@@ -156,5 +137,19 @@ func (this *LRUCache) Put(key int, value int) {
 	2.2 如果不存在，则：
 		2.2.1 如LRU长度未满，则添加到队首；
 		2.2.2 如LRU长度已满，则删除队尾节点，添加
+
+*/
+
+/**
+
+优化思路：
+
+如果完成“获取” / “添加” 要O(1)的时间复杂度，
+需要额外借助数据结构，O(1)直接联想到的是hash map。
+
+现在速度满的原因在于查找，
+当查找元素是否存在时，需要遍历列表，由此每次插入的时候，都需要遍历列表，时间复杂度为：O(n)。
+
+增加hash map，表示节点是否存在，如果存在的话，其value指向链表中的节点。
 
 */
--- 146_lru_cache/lru_cache_test.go
+++ 146_lru_cache/lru_cache_test.go
@@ -1,7 +1,6 @@
 package lru_cache
 
 import (
-	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,12 +8,8 @@ import (
 
 func TestLRUCache(t *testing.T) {
 	cache := Constructor(2)
-	log.Printf("cache init: %+v", cache.Println())
 	cache.Put(1, 1)
-	log.Printf("after put [1], cache init: %+v", cache.Println())
-
 	cache.Put(2, 2)
-	log.Printf("after put [1, 2], cache init: %+v", cache.Println())
 	assert.Equal(t, 1, cache.Get(1))  // 返回  1
 	cache.Put(3, 3)                   // 该操作会使得密钥 2 作废
 	assert.Equal(t, -1, cache.Get(2)) // 返回 -1 (未找到)
